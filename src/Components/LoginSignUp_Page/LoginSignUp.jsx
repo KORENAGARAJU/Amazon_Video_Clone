4806d6b10ca5c0d6d06910f4876847e9cff5e336
Nagaraju <nagaraj13562001@gmail>
2024-06-29 11:44:59 +0530

    new

--- src/Components/LoginSignUp_Page/LoginSignUp.jsx
+++ src/Components/LoginSignUp_Page/LoginSignUp.jsx
@@ -3,20 +3,24 @@ import Prime_logo from '../../assets/loginSignUpLogo.png';
 import { MdArrowRight } from "react-icons/md";
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+
 const LoginSignUp = () => {
+  const [IsSignUp , ToggleSignUpLogin] = useState(false) ;
+  const ToggleSignUpLoginHandler = ()=>ToggleSignUpLogin(!IsSignUp)
     const [isForgotOpen , isForgotOpenToggle] = useState(false);
     const ForgotPassHandler =()=>{
         isForgotOpenToggle(!isForgotOpen)
     }
         
   return (
-    <div className='Login-SignUp_content_wrapper'>
+    <div className='topContainer_lgnSign'>
+      <div className='Login-SignUp_content_wrapper'>
       <div className='LoginSignUp_inner'>
           <div className='img-container'>
-            <img style={{width:'150px'}} src={Prime_logo} alt='prime_logo'/>
+            <img className='prime_logo_cl' src={Prime_logo} alt='prime_logo'/>
           </div>
-          <div className='LoginSignUp_content'>
-              <h1>Sign in</h1>
+           <div className='LoginSignUp_content'>
+              {IsSignUp ? <h1>Sign Up</h1> : <h1>Sign in</h1>}
               <div className='Input_wrappper'>
                 <label className='input_header_label'>Email or mobile number</label>
                 <input placeholder='Enter Your Email' className='input_SignUp_login'/>
@@ -28,15 +32,33 @@ const LoginSignUp = () => {
               <div className='Need_help_Wrapper'>
                 <div className='Link_Acc-Header' onClick={ForgotPassHandler}>
                     <MdArrowRight style={{fontSize:'16px'}}/>
-                    <p>Need help</p>
+                    <p>Need help?</p>
                 </div>
                 {isForgotOpen && (<Link className='ForgotPassText' to={'/ForgotPassword'}>Forgot Password</Link>)}
               </div>
           </div>
-          
+          <div className='Divider-Container'>
+            <div className='line'></div>
+            <p className='NewToAmazon'>New to Amazon?</p>
+            <div className='line'></div>
+          </div>
+          <button className='CreateAccount_btn' type='submit' onSubmit={ToggleSignUpLoginHandler}>Create your Amazon account</button>
+          <div className='outer-line'>
       </div>
+      <div className='SignConditions_Links'>
+                            <Link to='/' className='link_Sign_F'>Terms and Privacy Notice</Link>
+                            <Link to='/' className='link_Sign_F'>Send us feedback</Link>
+                            <Link to='/' className='link_Sign_F'>Help</Link>
+      </div>
+      <p>© 1996-2024, Amazon.com, Inc. or its affiliates</p>
+      </div>
+      
     </div>
+   
+    </div>
+   
+    
   )
 }
 
-export default LoginSignUp
+export default LoginSignUp;
